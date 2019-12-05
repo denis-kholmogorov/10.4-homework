f5b84a4682b2daf6436ad24884a198557ac38b22
kholmogorov.denis <kholmogorov.denis@gmail.com>
2019-12-05 13:50:37 +0300

    version 1.0

---
+++ src/main/java/Main.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+        String url ="jdbc:mysql://localhost:3306/skillbox?" +
+                "useUnicode=true&useJDBCCompliantTimezoneShift=true&" +
+                "useLegacyDatetimeCode=false&serverTimezone=UTC";
+        String user = "root";
+        String pass = "23019088";
+        String sql_request = "SELECT name, COUNT(MONTH(Subscriptions.subscription_date))/" +
+                "COUNT(DISTINCT MONTH(Subscriptions.subscription_date)) as avgMonth " +
+                "FROM Courses JOIN Subscriptions ON Subscriptions.course_id = Courses.id " +
+                "GROUP BY Courses.name";
+        try {
+            Connection connection = DriverManager.getConnection(url, user, pass);
+
+        Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(sql_request);
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String avgMonth = resultSet.getString("avgMonth");
+                System.out.println(name + "  -  " + avgMonth);
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
